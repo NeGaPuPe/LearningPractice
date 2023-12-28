278db0d871b2ec79eadb3ae81016bbe5ac721389
Ng <rich416@mail.ru>
2023-12-28 04:30:16 +0300

    Files

--- PracticeShopProject/Model/ShopModel.Designer.cs
+++ PracticeShopProject/Model/ShopModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\Мария\Downloads\Practice-Version1.2\PracticeShopProject\Model\ShopModel.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Users\Мария\Downloads\Practice-Version1.3\PracticeShopProject\Model\ShopModel.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
